26ddabb300479decd16309e5bbdd01f9be7a12b9
Srdjan667 <73754173+Srdjan667@users.noreply.github.com>
2023-07-21 13:47:38 +0200

    test: new item_library view tests

--- item_library/tests/test_views.py
+++ item_library/tests/test_views.py
@@ -1,6 +1,9 @@
+from datetime import timedelta
+
 from django.contrib.auth.models import User
 from django.test import TestCase, Client
 from django.urls import reverse
+from django.utils import timezone
 
 from item_library.models import Item
 from item_library.views import FILTERS, DEFAULT_SESSION_KEYS
@@ -12,13 +15,29 @@ class ItemLibraryViewsTest(TestCase):
     def setUpTestData(cls):
         cls.user = User.objects.create_user("testuser", password="testing321")
 
-        cls.item = Item(title="Spear", 
-                        description="A shiny spear", 
-                        value=250, 
-                        rarity=1, 
-                        author=cls.user)
+        cls.common_item = Item(title="Spear", 
+                               description="A shiny spear", 
+                               value=250, 
+                               rarity=1, 
+                               date_created=timezone.now(),
+                               author=cls.user)
 
-        cls.item.save()
+        cls.rare_item = Item(title="Magic Wand", 
+                             description="A magical wand", 
+                             value=2000, 
+                             rarity=3, 
+                             date_created=timezone.now(), 
+                             author=cls.user)
+
+        time = timezone.now() + timedelta(days=30)
+        cls.future_item = Item(title="Axe", 
+                               description="Axe from future", 
+                               date_created=time,
+                               author=cls.user)
+
+        cls.common_item.save()
+        cls.rare_item.save()
+        cls.future_item.save()
 
     def setUp(self):
         self.client = Client()
@@ -27,18 +46,27 @@ class ItemLibraryViewsTest(TestCase):
         self.assertTrue(self.login_successful)
 
     def test_index_view_response(self):
-        response = self.client.get(reverse('item_library:index'), follow=True)
-        response_content = str(response.content)
+        response = self.client.get(reverse('item_library:index'))
 
         self.assertEqual(200, response.status_code)
-        # Check is everything is dispayed
-        self.assertIn(self.item.title, response_content)
-        self.assertIn(self.item.description, response_content)
-        # In database rarity is saved as 1, but it is displayed as Common
-        self.assertIn("Common", response_content)
+
+    def test_index_view_is_item_displayed(self):
+        response = self.client.get(reverse('item_library:index'))
+        response_content = str(response.content)
+
+        self.assertIn(self.common_item.title, response_content)
+        self.assertIn(self.common_item.description, response_content)
+
+    def test_index_view_is_future_item_displayed(self):
+        response = self.client.get(reverse('item_library:index'))
+        response_content = str(response.content)
+
+        # Items whose date_created is in future should not be displayed 
+        self.assertNotIn(self.future_item.title, response_content)
+        self.assertNotIn(self.future_item.description, response_content)
 
     def test_index_view_default_session_keys(self):
-        response = self.client.get(reverse('item_library:index'), follow=True)
+        response = self.client.get(reverse('item_library:index'))
         session_keys = self.client.session.keys()
 
         for f in FILTERS:
@@ -46,3 +74,21 @@ class ItemLibraryViewsTest(TestCase):
 
         for d in DEFAULT_SESSION_KEYS.keys():
             self.assertIn(d, self.client.session.keys())
+
+        # Make sure none of the values are checked
+        for r in self.client.session["rarity_list"]:
+            self.assertIsNone(r["is_checked"])
+
+    def test_index_view_is_filter_working(self):
+        form_data = {
+        "filter": "Filter",
+        "rare": "3"
+        }
+
+        response = self.client.post(reverse('item_library:index'), 
+                                    data=form_data)
+        response_content = str(response.content)
+
+        # Only items with "rare" rarity should be displayed
+        self.assertNotIn(self.common_item.title, response_content)
+        self.assertIn(self.rare_item.title, response_content)
