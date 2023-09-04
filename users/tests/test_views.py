d134c3e1c554f66478e00d2d5d9e04238e17b202
Srdjan667 <73754173+Srdjan667@users.noreply.github.com>
2023-09-04 21:46:29 +0200

    test: login view tests

--- users/tests/test_views.py
+++ users/tests/test_views.py
@@ -1,5 +1,6 @@
 from django.urls import reverse
-from django.test import TestCase, Client
+from django.contrib import auth
+from django.test import SimpleTestCase, TestCase, Client
 from django.contrib.auth.models import User
 
 class TestCustomRegisterView(TestCase):
@@ -29,7 +30,7 @@ class TestCustomRegisterView(TestCase):
 
 	def test_if_custom_register_view_rejects_invalid_user(self):
 		self.client = Client()
-		# Try to register without username
+		# If username is not provided, user should not be added to the database
 		data = {"email": "good@user.com", 
 				"password1": "secret_123", 
 				"password2": "secret_123"}
@@ -47,4 +48,41 @@ class TestCustomRegisterView(TestCase):
 		response = self.client.post(reverse('account:register'), data)
 
 		self.assertRedirects(response, reverse("account:login"))
-		
+
+
+class TestCustomLoginView(TestCase):
+	def test_login_view_status_code(self):
+		self.client = Client()
+		response = self.client.get(reverse('account:login'))
+		self.assertEquals(response.status_code, 200)
+
+
+	def test_login_view_uses_correct_template(self):
+		self.client = Client()
+		response = self.client.get(reverse('account:login'))
+		self.assertTemplateUsed(response, "users/login.html")
+
+
+	def test_user_can_log_in(self):
+		# Save user in database
+		User.objects.create_user(username="David", password="waterfall")
+		self.client = Client()
+		credentials = {"username": "David", "password": "waterfall"}
+
+		# This user should be able to log in
+		self.client.post(reverse("account:login"), credentials)
+		user = auth.get_user(self.client)
+
+		self.assertTrue(user.is_authenticated)
+
+
+	def test_user_can_not_log_in(self):
+		self.client = Client()
+		credentials = {"username": "Brian", "password": "platinum"}
+
+		# This user should not be able to log in becausse he is not in
+		# the database
+		self.client.post(reverse("account:login"), credentials)
+		user = auth.get_user(self.client)
+
+		self.assertFalse(user.is_authenticated)
