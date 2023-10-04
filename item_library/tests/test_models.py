77352733b000b41f453c3b5aa1a92cdfeb095dd9
Srdjan667 <73754173+Srdjan667@users.noreply.github.com>
2023-10-04 19:58:07 +0200

    test: Spell model tests

--- item_library/tests/test_models.py
+++ item_library/tests/test_models.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 
-from item_library.models import Item
+from item_library.models import Item, Spell
 
 
 class ItemModelTest(TestCase):
@@ -24,3 +25,42 @@ class ItemModelTest(TestCase):
         item = Item.objects.get(id=1)
         expected_name = item.title
         self.assertEqual(str(item), expected_name)
+
+
+class SpellModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create(username="testuser")
+        cls.spell = Spell.objects.create(
+            title="Levitate", school=7, level=4, author=cls.user
+        )
+
+    def test_spell_valid(self):
+        db_entry = Spell.objects.get(title="Levitate")
+
+        self.assertEqual(db_entry.title, "Levitate")
+        # full_clean() returns None if no exceptions are raised
+        self.assertIsNone(self.spell.full_clean())
+
+    def test_invalid_spell_school(self):
+        school_below_minimum_value = Spell(
+            title="Minor Ilusion", school=-1, author=self.user
+        )
+        school_above_maximum_value = Spell(
+            title="Invisibility", school=9, author=self.user
+        )
+        self.assertRaises(ValidationError, school_below_minimum_value.full_clean)
+        self.assertRaises(ValidationError, school_above_maximum_value.full_clean)
+
+    def test_invalid_spell_level(self):
+        level_below_minimum_value = Spell(title="Fog", level=-1, author=self.user)
+        level_above_maximum_value = Spell(
+            title="Arcane Gate", level=10, author=self.user
+        )
+
+        self.assertRaises(ValidationError, level_below_minimum_value.full_clean)
+        self.assertRaises(ValidationError, level_above_maximum_value.full_clean)
+
+    def test_school_and_value_displayed_correctly(self):
+        self.assertEqual(self.spell.get_school_display(), "Necromancy")
+        self.assertEqual(self.spell.get_level_display(), "4th")
