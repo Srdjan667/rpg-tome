289a0d2586c9062c75a3141afd52dfce7df91336
Srdjan667 <73754173+Srdjan667@users.noreply.github.com>
2023-10-05 19:48:55 +0200

    feat: added Spell form

--- item_library/forms.py
+++ item_library/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.core.validators import MinValueValidator
 from django.forms import ModelForm
 
-from .models import Item
+from item_library.models import Item, Spell
 
 
 class ItemsForm(ModelForm):
@@ -37,6 +37,52 @@ class ItemsForm(ModelForm):
 
     def clean_value(self):
         data = self.cleaned_data["value"]
+        if data is None:
+            return 0
+        return data
+
+
+class SpellsForm(ModelForm):
+    title = forms.CharField(
+        widget=forms.TextInput(
+            attrs={"placeholder": "Enter title here", "autofocus": True}
+        ),
+        required=True,
+    )
+
+    description = forms.CharField(
+        widget=forms.Textarea(attrs={"placeholder": "Enter description here"}),
+        required=False,
+    )
+
+    school = forms.IntegerField(
+        widget=forms.Select(
+            choices=Spell.SCHOOLS_OF_MAGIC,
+            attrs={"placeholder": "Select school of magic"},
+        ),
+        required=False,
+    )
+
+    level = forms.IntegerField(
+        widget=forms.Select(
+            choices=Spell.SPELL_LEVELS,
+            attrs={"placeholder": "Select level of your spell"},
+        ),
+        required=False,
+    )
+
+    class Meta:
+        model = Spell
+        fields = ["title", "description", "school", "level"]
+
+    def clean_school(self):
+        data = self.cleaned_data["school"]
+        if data is None:
+            return 0
+        return data
+
+    def clean_level(self):
+        data = self.cleaned_data["level"]
         if data is None:
             return 0
         return data
