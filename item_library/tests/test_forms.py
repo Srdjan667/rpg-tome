dc01b60f7d5990992b8da9139158c4fd48bf5e1a
Srdjan667 <73754173+Srdjan667@users.noreply.github.com>
2023-10-05 13:06:33 +0200

    test: Spell form tests

--- item_library/tests/test_forms.py
+++ item_library/tests/test_forms.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from item_library.forms import ItemsForm
+from item_library.forms import ItemsForm, SpellsForm
 
 RARITIES = ["Common", "Uncommon", "Rare", "Very Rare", "Legendary", "Artifact"]
 
@@ -69,5 +69,29 @@ class ItemsFormTest(TestCase):
 
         form.is_valid()
 
-        # Make sure value is defaulted to 0 instead of None
+        # Make sure value is 0 when nothing is provided
         self.assertEqual(form.cleaned_data["value"], 0)
+
+
+class SpellsFormTest(TestCase):
+    def test_form_is_valid(self):
+        form_data = {
+            "title": "Arcane Gate",
+            "description": "Open gate to another world",
+            "school": 4,
+            "level": 4,
+        }
+        form = SpellsForm(data=form_data)
+
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_form(self):
+        form_data = {
+            # The title must be present if the form is to be valid
+            "description": "Open gate to another world",
+            "school": 4,
+            "level": 4,
+        }
+        form = SpellsForm(data=form_data)
+
+        self.assertFalse(form.is_valid())
